7eec51b6da7d1bbfca3e1cc9abd044c700208631
sergeichestakov <schestakov@ucdavis.edu>
2020-07-18 17:14:55 -0700

    Add basic camera layout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = React.useState<boolean | null>(
+    null
+  );
+  const [type, setType] = React.useState(Camera.Constants.Type.back);
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.cameraContainer} type={type}>
+        <View style={styles.cameraView}>
+          <TouchableOpacity
+            style={styles.flipButtonContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.flipButtonText}> Flip </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.captureButton}
+            onPress={() => {
+              console.log("taking picture");
+            }}
+          ></TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -14,8 +52,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  cameraContainer: {
+    flex: 1,
+  },
+  cameraView: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    backgroundColor: "transparent",
+  },
+  captureButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 5,
+    borderColor: "#FFF",
+    marginBottom: 50,
+  },
+  flipButtonContainer: {
+    position: "absolute",
+    left: 20,
+    bottom: 30,
+  },
+  flipButtonText: {
+    fontSize: 32,
+    color: "white",
   },
 });
